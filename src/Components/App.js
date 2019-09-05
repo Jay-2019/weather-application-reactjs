7b7f02ebb9b2dc8e158c2c3ea469336422652d14
jay <jay.mauryavanshi2014@gmail.com>
2019-09-05 14:17:25 +0530

    first commit

---
+++ src/Components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Searchbar from './Searchbar';
+class App extends React.Component {
+
+    render() {
+
+        return <Searchbar />
+    }
+}
+export default App;
---
+++ src/Components/Searchbar.js
@@ -0,0 +1,10 @@
+import { Component } from "react";
+
+class Searchbar extends Component {
+
+    render() {
+
+        return "this is search component"
+    }
+}
+export default Searchbar;
