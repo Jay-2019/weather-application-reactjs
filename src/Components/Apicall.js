5c3408ce2486b41041c06f3e25efe360bfb95566
manish-nx <manish.kumar@successive.tech>
2019-09-19 12:51:10 +0530

    update api call

--- src/Components/Apicall.js
+++ src/Components/Apicall.js
@@ -1,32 +1,8 @@
-import React from 'react'
-
-class Apicall extends React.Component {
-
-    state = {
-        getCityName: this.props.setCityName,
-    }
-
-
-    Apicall = async (e) => {
-
-        // const cityName = this.props.setCityName;
-        const cityName = this.state.getCityName;
-        console.log(cityName);
-        const getReasponse = await fetch(`api.openweathermap.org/data/2.5/weather?q=${cityName}appid=a0dee3ad64641dc88ea823ed96aaec61`);
-        const getWeather = await getReasponse.json();
-        console.log(getWeather);
-        // send  getWather to app.js using a call back method
-        // getWeather is available as a props for display weather in the Displayweather.js component
-        return
-    }
-
-    render() {
-
-        return (
-            <React.StrictMode>
-                api calling...
-            </React.StrictMode>
-        )
+class Apicall {
+    fetchWeatherDetails = async (city, callback) => {
+        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a0dee3ad64641dc88ea823ed96aaec61`);
+        const weatherData = await response.json();
+        callback(weatherData);
     }
 }
-export default Apicall;
+export default new Apicall();
--- src/Components/App.js
+++ src/Components/App.js
@@ -2,24 +2,55 @@ import React from 'react';
 import style from "../style.module.css";
 import { Container, Row, Navbar, Col } from 'react-bootstrap';
 import logo from "./logo192.png";
-import Apicall from "./Apicall";
+import apiCall from "./Apicall";
 import Searchbar from './Searchbar';
 
 class App extends React.Component {
 
     state = {
+        isLoading: false,
         getCityName: '',
+        humidity: '',
+        temp: '',
+        pressure: '',
     }
 
     //call-back function for get data from search Bar...
 
     getDataFromSearchbar = (getData) => {
-        this.setState({ getCityName: getData });
+        this.setState({ getCityName: getData, isLoading: true });
         console.log(getData);
-        // Apicall(this.state.cityName);
+        apiCall.fetchWeatherDetails(getData, this.setWeatherDetails);
     }
 
+    setWeatherDetails = (data = {}) => {
+        const { main = {}} = data;
+        const { humidity, temp, pressure } = main;
+        this.setState({
+            isLoading: false,
+            humidity,
+            temp,
+            pressure,
+        });
+    }
 
+    renderWeatherDetails = () => {
+        const { isLoading } = this.state;
+        if (isLoading) {
+            return <h4>API Calling...</h4>;
+        }
+        const { humidity='', temp='', pressure=''} = this.state;
+        if (!(humidity || temp || pressure)) {
+            return null;
+        }
+        return (
+            <>
+                <div>{`Humidity: ${humidity}`}</div>
+                <div>{`Temperature: ${temp}`}</div>
+                <div>{`Pressure: ${pressure}`}</div>
+            </>
+        );
+    }
 
     render() {
 
@@ -58,9 +89,7 @@ class App extends React.Component {
                     </Col>
                 </Row>
 
-                {/* Here we Render Api-component for Json Response */}
-                <Apicall setCityName={this.state.getCityName} />
-
+                {this.renderWeatherDetails()}
 
                 {/* this is a body of application  */}
                 <Row>
